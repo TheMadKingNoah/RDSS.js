90a14d701b55aff1332fd481675cd93a32e493ee
Noah <s1147965@student.windesheim.nl>
2022-07-03 23:38:27 +0200

    removed unreleased functions. Will make a seperate branch instead

--- src/commands/QuickMute.ts
+++ src/commands/QuickMute.ts
@@ -3,8 +3,8 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("quickmute")
-        .setDescription("QuickMutes the person"),
+        .setDescription("QuickMutes the user"),
     async excecute(interaction: { reply: (arg0: string) => void; }){
-        interaction.reply("quick")
+    
     }
 }
