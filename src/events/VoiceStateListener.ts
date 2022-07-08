7b89acc9c45ffff4f86c86184b0eb129813bef90
Noah <noahkok123@gmail,com>
2022-07-08 22:22:47 +0200

    bug fix

--- src/events/VoiceStateListener.ts
+++ src/events/VoiceStateListener.ts
@@ -9,33 +9,33 @@ module.exports = {
         let newVoiceChannel = (newState.channel as VoiceChannel)
         let oldVoiceChannel = (oldState.channel as VoiceChannel)
 
-        const voiceLogsChannel = oldState.client.channels.cache.get(Properties.VOICE_LOGS_CHANNEL_ID);
+        oldState.client.channels.fetch(Properties.VOICE_LOGS_CHANNEL_ID).then( voiceLogsChannel => { 
+            if (voiceLogsChannel != null) {
 
-        if (voiceLogsChannel != null) {
-
-            if (newVoiceChannel !== null && oldVoiceChannel === null) {
-
-                if (newState.member != null) {
-
-                    (voiceLogsChannel as TextChannel).send({ embeds: [EmbedBuilds.getOnVoiceChannelJoinEmbed(newState)] })
-                }
-
-            } else if (newVoiceChannel !== null && oldVoiceChannel !== null) {
-
-                if (newState.member != null && newState.member != null) {
-
-                    (voiceLogsChannel as TextChannel).send({ embeds: [EmbedBuilds.getOnVoiceChannelChangeEmbed(oldState, newState)] })
-
-                }
-
-            } else if (newVoiceChannel === null && oldVoiceChannel !== null) {
-
-                if (oldState.member != null) {
-
-                    (voiceLogsChannel as TextChannel).send({ embeds: [EmbedBuilds.getOnVoiceChannelLeaveEmbed(oldState)] })
-
+                if (newVoiceChannel !== null && oldVoiceChannel === null) {
+    
+                    if (newState.member != null) {
+    
+                        (voiceLogsChannel as TextChannel).send({ embeds: [EmbedBuilds.getOnVoiceChannelJoinEmbed(newState)] })
+                    }
+    
+                } else if (newVoiceChannel !== null && oldVoiceChannel !== null) {
+    
+                    if (newState.member != null && newState.member != null) {
+    
+                        (voiceLogsChannel as TextChannel).send({ embeds: [EmbedBuilds.getOnVoiceChannelChangeEmbed(oldState, newState)] })
+    
+                    }
+    
+                } else if (newVoiceChannel === null && oldVoiceChannel !== null) {
+    
+                    if (oldState.member != null) {
+    
+                        (voiceLogsChannel as TextChannel).send({ embeds: [EmbedBuilds.getOnVoiceChannelLeaveEmbed(oldState)] })
+    
+                    }
                 }
             }
-        }
+        }).catch( e => {})
     }
 }
