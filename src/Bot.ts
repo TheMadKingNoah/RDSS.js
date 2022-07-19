6af1aab09015e2e628016fe044b10e118d531641
Archasion <james.nswork@gmail.com>
2022-07-19 20:26:48 +0100

    Added `Bot` subclass to `Client` (for handling tasks)

--- src/Bot.ts
+++ src/Bot.ts
@@ -6,26 +6,49 @@ import "dotenv/config";
 
 console.log("Bot is starting...");
 
-const client = new Client({
-    intents: ["GUILDS", 'GUILD_MESSAGE_REACTIONS', "GUILD_MEMBERS", "GUILD_VOICE_STATES", "GUILD_MESSAGES"],
-    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-});
+export default class Bot extends Client {
+    // commands!: CommandHandler;
 
-let eventFiles;
-if (process.env.BOT_TOKEN == "Production") {
-    eventFiles = fs.readdirSync(path.join(__dirname, "./dist/events")).filter((file: string) => file.endsWith(".js"));
-} else {
-    eventFiles = fs.readdirSync(path.join(__dirname, "./events")).filter((file: string) => file.endsWith(".ts"));
-}
+    constructor() {
+        super({
+            intents: [
+                "GUILDS",
+                'GUILD_MESSAGE_REACTIONS',
+                "GUILD_MEMBERS",
+                "GUILD_VOICE_STATES",
+                "GUILD_MESSAGES"],
+            partials: [
+                "MESSAGE",
+                "CHANNEL",
+                "REACTION"
+            ],
+        });
 
-for (const file of eventFiles) {
-    const event = require(path.join(__dirname, `./events/${file}`));
+        (async () => {
+            // Event Handler
+            let eventFiles;
 
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args));
+            if (process.env.BOT_TOKEN == "Production") {
+                eventFiles = fs.readdirSync(path.join(__dirname, "./dist/events")).filter((file: string) => file.endsWith(".js"));
+            } else {
+                eventFiles = fs.readdirSync(path.join(__dirname, "./events")).filter((file: string) => file.endsWith(".ts"));
+            }
+
+            for (const file of eventFiles) {
+                const event = require(path.join(__dirname, `./events/${file}`));
+
+                if (event.once) {
+                    this.once(event.name, (...args) => event.execute(...args));
+                } else {
+                    this.on(event.name, (...args) => event.execute(...args));
+                }
+            }
+
+            // this.commands = new CommandHandler(this);
+
+            this.login(process.env.BOT_TOKEN);
+        })();
     }
 }
 
-client.login(process.env.BOT_TOKEN);
+new Bot();
