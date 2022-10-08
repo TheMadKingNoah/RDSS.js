2fde5c5718be05c36fe9d7b03e0b745649003d44
TheMadKingNoah <75645597+TheMadKingNoah@users.noreply.github.com>
2022-10-08 12:03:52 +0200

    Update StoreToMediaLogs.ts
    requested space

--- src/interactions/contexts/StoreToMediaLogs.ts
+++ src/interactions/contexts/StoreToMediaLogs.ts
@@ -28,7 +28,7 @@ export default class QuickMute30mCommand extends ContextMenu {
                 messageAttachments.push(new MessageAttachment(media.attachment));
             });
 
-            logChannel.send({content: `Media-logs by ${interaction.user}(\`${interaction.user.username}#${interaction.user.discriminator}\`) <t:${Math.trunc(message.createdTimestamp/1000)}:F>`, 
+            logChannel.send({content: `Media-logs by ${interaction.user} (\`${interaction.user.username}#${interaction.user.discriminator}\`) <t:${Math.trunc(message.createdTimestamp/1000)}:F>`, 
             files:messageAttachments,
             allowedMentions:undefined
         }).then(mediaLogMessage => {
