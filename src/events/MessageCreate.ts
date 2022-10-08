a6be4aa7d5838f2de87eee4be97b9b405701ad44
Nick <james.nswork@gmail.com>
2022-10-08 21:46:32 +0100

    Implemented an event winner queue

---
+++ src/events/MessageCreate.ts
@@ -0,0 +1,91 @@
+import Properties from "../utils/Properties";
+import EventListener from "../modules/events/Event";
+import Bot from "../Bot";
+
+import {
+    TextChannel,
+    GuildMember,
+    Message,
+    MessageButton,
+    MessageActionRow, MessageEmbed, MessageSelectMenu
+} from "discord.js";
+
+module.exports = class MessageCreateEventListener extends EventListener {
+    constructor(client: Bot) {
+        super(client, {
+            name: "messageCreate",
+            once: false
+        });
+    }
+
+    async execute(message: Message) {
+        if (message.channelId === Properties.channels.winnerQueue) {
+            if (message.author.bot) return;
+
+            const winnerQueue = await message.guild?.channels.fetch(Properties.channels.winnerQueue) as TextChannel;
+            if (!winnerQueue) return;
+
+            const mentions = message.mentions.members;
+
+            if (mentions?.size === 0 || !mentions) {
+                const deleteMessage = new MessageButton()
+                    .setCustomId("deleteMessage")
+                    .setLabel("Delete Reply")
+                    .setStyle("DANGER");
+
+                const actionRow = new MessageActionRow().setComponents(deleteMessage);
+
+                message.reply({
+                    content: "There are no mentions in your message!",
+                    components: [actionRow]
+                }).catch(console.error);
+                return;
+            }
+
+            const memberList = mentions?.map((member: GuildMember) => `${member} (\`${member.id}\`)`).join("\n");
+            let winnerRoles = await message.guild?.roles.fetch();
+
+            if (!winnerRoles) return;
+            const winnerRoleList: { label: string, value: string }[] = []
+
+            winnerRoles = await winnerRoles.filter((role) => role.name.includes("Winner") || role.name.includes("Champion"));
+            winnerRoles.forEach((role) => {
+                const year = role.name.match(/\d{4}/g)?.[0];
+                const winnerRole = {
+                    label: role.name.replace(/\d+/g, "").trim(),
+                    value: role.id
+                }
+
+                // @ts-ignore
+                if (year) winnerRole.description = year;
+                winnerRoleList.push(winnerRole);
+            });
+
+            const roleOptions = new MessageSelectMenu()
+                .setCustomId("selectWinnerRole")
+                .setPlaceholder("Select a role to award...")
+                .setOptions(winnerRoleList);
+
+            const actionRow = new MessageActionRow().setComponents(roleOptions);
+
+            const embed = new MessageEmbed()
+                .setAuthor({
+                    name: `Requested by ${message.author.tag} (${message.member?.nickname})`,
+                    iconURL: message.author.displayAvatarURL()
+                })
+                .setFields([{
+                    name: "Winners",
+                    value: memberList as string
+                }])
+                .setFooter({ text: `ID: ${message.author.id}` })
+                .setTimestamp()
+
+            winnerQueue.send({
+                embeds: [embed],
+                components: [actionRow]
+            }).catch(console.error);
+
+            message.delete().catch(console.error);
+        }
+    }
+}
---
+++ src/interactions/buttons/ForceRemoveWinnerRoles.ts
@@ -0,0 +1,48 @@
+import Button from "../../modules/interactions/buttons/Button";
+import Bot from "../../Bot";
+
+import {ButtonInteraction, GuildMember, Message} from "discord.js";
+import RoleUtils from "../../utils/RoleUtils";
+
+export default class ForceRemoveWinnerRolesButton extends Button {
+    constructor(client: Bot) {
+        super(client, {
+            name: "forceRemoveWinnerRoles"
+        });
+    }
+
+    async execute(interaction: ButtonInteraction) {
+        if (!RoleUtils.hasAnyRole(interaction.member as GuildMember, [
+            RoleUtils.roles.publicSectorPm,
+            RoleUtils.roles.seniorModerator,
+            RoleUtils.roles.manager
+        ])) {
+            interaction.reply({
+                content: "You do not have permission to use this button!",
+                ephemeral: true
+            }).catch(console.error);
+            return;
+        }
+
+        let removedRoles = 0;
+        const winners = this.client.winners.list.filter(winner => winner.messageId === interaction.message.id);
+        let winnerRole = "winner";
+
+        for (const [winnerId, data] of winners.entries()) {
+            const member = await interaction.guild?.members.fetch(winnerId).catch(console.error) as GuildMember;
+            clearTimeout(data.timeout);
+
+            this.client.winners.list.delete(winnerId);
+            member.roles.remove(data.roleId).catch(console.error);
+            winnerRole = `<@&${data.roleId}>`;
+            removedRoles++;
+        }
+
+        (interaction.message as Message).delete().catch(console.error);
+
+        await interaction.reply({
+            content: `Removed **${removedRoles}** ${winnerRole} roles!`,
+            ephemeral: true
+        });
+    }
+}
