74c3c5c6ae8af3e9a5791bf9c52f0f2ffe03c4be
Noah <noahkok123@gmail,com>
2022-10-24 19:13:24 +0200

    added more logs

--- src/interactions/buttons/ClearModAlert.ts
+++ src/interactions/buttons/ClearModAlert.ts
@@ -2,7 +2,9 @@ import Button from "../../modules/interactions/buttons/Button";
 import ModAlert from "../../utils/ModAlert";
 import Bot from "../../Bot";
 
-import { ButtonInteraction, Message } from "discord.js";
+import { ButtonInteraction, GuildMember, Message, ThreadChannel } from "discord.js";
+import RoleUtils from "../../utils/RoleUtils";
+import Properties from "../../utils/Properties";
 
 export default class ClearModAlertButton extends Button {
     constructor(client: Bot) {
@@ -19,5 +21,14 @@ export default class ClearModAlertButton extends Button {
             .replace(/\D/g, '');
 
       ModAlert.deleteModAlert(messageId, modAlertMessage, null);
+
+      
+       //trial phase
+       if(!interaction.member) return;
+       if (RoleUtils.hasAnyRole(interaction.member as GuildMember, [RoleUtils.roles.trialModerator])) {
+        interaction.client.channels.fetch(Properties.channels.trialLogs).then(channel => {
+            (channel as ThreadChannel).send(`${interaction.member} approved a mod-alert: \n\n \`\`\`${modAlertMessage.content}\`\`\``)
+        })
+    }
     }
 }
