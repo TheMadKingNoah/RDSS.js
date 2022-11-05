c227fd4a286ad7173c3dd5f828691a1bb7baaa30
Nick <james.nswork@gmail.com>
2022-11-05 18:45:34 +0000

    Credit the user that accepted the request

--- src/interactions/select_menus/SelectWinnerRole.ts
+++ src/interactions/select_menus/SelectWinnerRole.ts
@@ -131,6 +131,7 @@ export default class SelectWinnerRoleSelectMenu extends SelectMenu {
         const actionRow = new MessageActionRow().setComponents(removeRoles);
         const editedEmbed = new MessageEmbed(interaction.message.embeds[0])
             .setColor(roleProperties.color)
+            .setDescription(`Accepted by ${interaction.user}`)
             .setFields([
                 {
                     name: `${roleProperties.name}${timestamp}`,
