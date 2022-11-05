a057219d7aaa670c0d5cfd41279771864ae09988
Nick <james.nswork@gmail.com>
2022-11-05 18:34:56 +0000

    Don't remove timed roles if other requests are present, reset timer to that request

--- src/interactions/buttons/ForceRemoveWinnerRoles.ts
+++ src/interactions/buttons/ForceRemoveWinnerRoles.ts
@@ -1,8 +1,8 @@
 import Button from "../../modules/interactions/buttons/Button";
+import RoleUtils from "../../utils/RoleUtils";
 import Bot from "../../Bot";
 
 import {ButtonInteraction, GuildMember, Message} from "discord.js";
-import RoleUtils from "../../utils/RoleUtils";
 
 export default class ForceRemoveWinnerRolesButton extends Button {
     constructor(client: Bot) {
@@ -24,17 +24,49 @@ export default class ForceRemoveWinnerRolesButton extends Button {
             return;
         }
 
+        const remainingTimesList = new Map();
+
+        await interaction.channel?.messages.fetch({limit: 100})
+        .then(async messages => {
+            for (const message of messages.values()) {
+                if (!message.author.bot) continue;
+                if (message.embeds.length === 0) continue;
+                if (!message.content) continue;
+                if (message.id === interaction.message.id) continue;
+
+                const roleId = message.content.replace(/\D/g, "");
+
+                const currentTime = Math.trunc(Date.now() / 1000);
+                const remainingTime = message.embeds[0].fields[0].name?.match(/\d{10,}/g)?.map(Number)[0] as number - currentTime;
+
+                const winnerList = message.embeds[0].fields[0].value;
+                const winnerIds = winnerList.match(/(?<=`)\d{17,19}(?=`)/g) as string[];
+
+                for await (const winnerId of winnerIds) {
+                    const roleTimer = remainingTimesList.get(`${winnerId}_${roleId}`);
+                    if (!roleTimer || roleTimer < remainingTime) remainingTimesList.set(`${winnerId}_${roleId}`, { duration: remainingTime, messageId: message.id });
+                }
+            }
+        })
+        .catch(console.error);
+
         let removedRoles = 0;
         const winners = this.client.winners.list.filter(winner => winner.messageId === interaction.message.id);
         let winnerRole = "winner";
 
         for (let [winnerId, data] of winners.entries()) {
             winnerId = winnerId.split("_")[0];
-
             const member = await interaction.guild?.members.fetch(winnerId).catch(console.error) as GuildMember;
-            clearTimeout(data.timeout);
 
+            clearTimeout(data.timeout);
             this.client.winners.list.delete(winnerId);
+
+            const winner = remainingTimesList.get(`${winnerId}_${data.roleId}`);
+            if (winner) {
+                this.client.winners.add(member, winner.messageId, data.roleId, winner.duration);
+                continue;
+            }
+
             member.roles.remove(data.roleId).catch(console.error);
             winnerRole = `<@&${data.roleId}>`;
             removedRoles++;
